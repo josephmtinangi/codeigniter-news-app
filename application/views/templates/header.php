fe8e6830345ae07f3bc40996f928f993b3fa028f
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-24 11:35:39 +0300

    Use bootstrap

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,7 +5,9 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title><?=$title?></title>
+		<link rel="stylesheet" type="text/css" href="<?=base_url(). '/assets/css/bootstrap.min.css';?>">
 	</head>
 	<body>
 		<?=$title?>
+
 	
