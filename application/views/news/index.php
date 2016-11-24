b56d9b1bc01f0c3ac7d6f3d465a49b56029ffe0a
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-24 10:55:43 +0300

    Display all news

---
+++ application/views/news/index.php
@@ -0,0 +1,11 @@
+<h2><?=$title?></h2>
+
+<?php foreach($news as $news_item): ?>
+	<h3><?=$news_item['title']?></h3>
+	<div class="news-content">
+		<?=$news_item['content']?>
+	</div>
+	<p>
+		<a href="<?=site_url('news/'.$news_item['slug'])?>">View article</a>
+	</p>
+<?php endforeach ?>	
